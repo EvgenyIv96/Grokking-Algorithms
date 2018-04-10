7afd6e203761d5fbe3305f7f285c1300935cbfeb
Евгений Иванов <eivanov@baccasoft.ru>
2018-04-10 17:42:29 +0300

    Добавлена функция, реализующая алгоритм бинарного поиска.

--- GrokkingAlgorithms.playground/Contents.swift
+++ GrokkingAlgorithms.playground/Contents.swift
@@ -1,3 +1,51 @@
+
+/// Function is used to determine index of given value in a given array. If there isn't given value in the given array function return nil.
+///
+/// - Parameters:
+///   - value: Given value
+///   - array: Given array
+/// - Returns: Index of given value in the given array. May be nil if there insn't given value in the given array.
+func findIndex<Item>(of value: Item, in array: [Item]) -> Int?
+    where Item: Comparable {
+    
+    let count =  array.count
+    
+    var lowIndex = 0
+    var highIndex = count - 1
+    
+    while lowIndex <= highIndex {
+        
+        let middleIndex = (lowIndex+highIndex)/2
+        let middleValue: Item = array[middleIndex]
+        
+        if middleValue == value {
+            return middleIndex
+        } else if middleValue < value {
+            lowIndex = middleIndex + 1
+        } else if middleValue > value {
+            highIndex = middleIndex - 1
+        }
+        
+    }
+    
+    return nil
+    
+}
+
+let numbers = [1, 3, 5, 7, 11]
+let strings = ["a", "b", "c", "z"]
+
+findIndex(of: 3, in: numbers)
+findIndex(of: 1, in: numbers)
+findIndex(of: 11, in: numbers)
+findIndex(of: 99, in: numbers)
+findIndex(of: 4, in: numbers)
+
+findIndex(of: "a", in: strings)
+findIndex(of: "z", in: strings)
+findIndex(of: "c", in: strings)
+findIndex(of: "y", in: strings)
+
 /*:
  # Упражнения:
  
