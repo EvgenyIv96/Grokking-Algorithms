9ef0085f716587cf32220a41b3f041c510bdcdf7
Евгений Иванов <eivanov@baccasoft.ru>
2018-04-12 13:20:14 +0300

    Добавлены упражнения

--- GrokkingAlgorithms.playground/Pages/Recursion.xcplaygroundpage/Contents.swift
+++ GrokkingAlgorithms.playground/Pages/Recursion.xcplaygroundpage/Contents.swift
@@ -1,6 +1,9 @@
 //: [Selection sorting](@previous)
 import Foundation
 
+/// Function is used to perfrorm countdown in console from given integer value to zero.
+///
+/// - Parameter point: Initial integer value.
 func countdown(_ point: Int) {
     
     print(point)
@@ -13,6 +16,10 @@ func countdown(_ point: Int) {
     
 }
 
+/// Function is used to calculate factorial for given integer value.
+///
+/// - Parameter value: Integer value which is used to calculate factorial for.
+/// - Returns: Factorial for given value.
 func factorial(_ value: Int) -> Int {
     
     if value == 1 {
@@ -23,5 +30,83 @@ func factorial(_ value: Int) -> Int {
     
 }
 
+/*:
+ # Упражнения
+ 
+ **4.1** Напишите код для функции sum (см. выше).
+ */
+
+/// Function is used to find sum of all numeric values in given array.
+///
+/// - Parameter array: Numbers array.
+/// - Returns: Sum value.
+func sum<T: Numeric>(_ array: [T]) -> T {
+    
+    if let element = array.first {
+        var tail = array
+        tail.remove(at: 0)
+        return element + sum(tail)
+    } else {
+        return 0
+    }
+    
+}
+
+/*:
+ **4.2** Напишите рекурсивную функцию для подсчета элементов в списке.
+ */
+
+/// Function is used to calculate elements count in the given array.
+///
+/// - Parameter array: Given array.
+/// - Returns: Count of elements in given array.
+func count(_ array: [Any]) -> Int {
+    
+    if let _ = array.first {
+        var tail = array
+        tail.remove(at: 0)
+        return 1 + count(tail)
+    } else {
+        return 0
+    }
+    
+}
+
+/*:
+ **4.3** Найдите наибольшее число в списке.
+ */
+
+/// Function is used to find biggest value in given numeric array. If given array will be empty function returns nil.
+///
+/// - Parameter array: Numeric array.
+/// - Returns: Biggest value in given array.
+func findBiggest<T: Comparable>(_ array: [T])  -> T? {
+    
+    if let element = array.first {
+        var tail = array
+        tail.remove(at: 0)
+        
+        guard let biggestInTail = findBiggest(tail) else { return element }
+        
+        return element > biggestInTail ? element : biggestInTail
+        
+    } else {
+        return nil
+    }
+    
+}
+
+/*:
+ **4.4** Помните бинарный поиск из главы 1? Он тоже относится к классу алгоритмов •разделяй и властвуй•. Сможете ли вы определить базовый и рекурсивный случай для бинарного поиска?
+ 
+ **Ответ:** Базовый случай - Массив из одного элемента. Если он совпадает с искомым - элемент найден, в противном случае - в массиве нет искомого элемента.
+ Рекурсивный случай - Половина исходного массива для которого дальше следует проводить бинарный поиск.
+ */
+
 countdown(5)
 factorial(5)
+sum([1, 2, 3])
+count([])
+count([1])
+count(["hello", "bye", "1233", "555"])
+findBiggest([1, 11, 3, 5, 3, 8])
